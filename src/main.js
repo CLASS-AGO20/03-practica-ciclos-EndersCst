3032ecc7850ebd432ccd1205279a6bf0bc6fb2dc
EndersCst <73366481+EndersCst@users.noreply.github.com>
2020-12-09 16:46:05 -0600

    implementedconvertirAString

--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
-export default class App {}
+export default class App {
+factorial(numero) {
+    let dato = numero;
+    let resultado = numero;
+    for (let i = dato-1; i >= 1; i--){
+        resultado = resultado * i
+    } 
+        
+    return resultado
+    
+}
+convertirAString(numero) {
 
+    let i =1;
+
+    while(i<=numero){
+        s = s + "*"
+        i = i + 1;
+    }
+
+    return s;
+
+}
+}
 let app = new App();
+
+console.log(app.factorial(5));
+
+console.log(app.convertirAString(6));
